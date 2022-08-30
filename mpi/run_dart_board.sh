015be59ecfcd07c3e8ad9cfee49d353eaae6827d
Nelson Earle <nelson.earle137@gmail.com>
2022-08-30 16:16:57 -0500

    add mpi examples

---
+++ mpi/run_dart_board.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+#  SBATCH CONFIG
+#-------------------------------------------------------------------------------
+## resources
+#SBATCH --partition hpc3
+#SBATCH --nodes=2
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1  # cores per task; set to one
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time 0-00:05:00
+#SBATCH --job-name=example_mpi_job
+#SBATCH --output=results-mpi-%j.out
+#-------------------------------------------------------------------------------
+
+# Load your modules here:
+module load openmpi/openmpi-3.1.4
+module list
+
+# Science goes here:
+srun ./pi_estimator
